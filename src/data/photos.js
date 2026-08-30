f3e0fdfbd6e4627fa7e9b6cced5000fe15ec4bb3
guptayuraj10 <guptayuraj10@gmail.com>
2026-08-30 19:55:12 +0545

    font changes

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,6 +1,13 @@
 // src/data/photos.js
 export const photoVouchers = [
   {
+    id: "MEM000",
+    title: "Album",
+    category: "Memories forever",
+    url: "/pic/album.jpeg"
+  },
+  {
+    
     id: "MEM001",
     title: "Group Selfie",
     category: "Training Sessions",
@@ -92,75 +99,118 @@ export const photoVouchers = [
   },
   {
     id: "MEM016",
-    title: "Konda Group",
-    category: "3 Idiots",
-    url: "/pic/konda.jpeg"
+    title: "Group Selfie",
+    category: "Selfi 📷 Time⏱️",
+    url: "/pic/swe.jpg"
   },
   {
     id: "MEM017",
     title: "Konda Group",
     category: "3 Idiots",
-    url: "/pic/k.jpeg"
+    url: "/pic/konda.jpeg"
   },
   {
     id: "MEM018",
     title: "Konda Group",
     category: "3 Idiots",
-    url: "/pic/k1.jpeg"
+    url: "/pic/k.jpeg"
   },
   {
     id: "MEM019",
     title: "Konda Group",
     category: "3 Idiots",
-    url: "/pic/konda2.jpeg"
+    url: "/pic/k1.jpeg"
   },
   {
     id: "MEM020",
+    title: "Konda Group",
+    category: "3 Idiots",
+    url: "/pic/konda2.jpeg"
+  },
+  {
+    id: "MEM021",
     title: "Triplets",
     category: "3 Idiots",
     url: "/pic/ven.jpeg"
   },
   {
-    id: "MEM021",
+    id: "MEM022",
     title: "Battery Saver & Spotify",
     category: "Singer & Dancer",
     url: "/pic/aka.jpeg"
   },
   {
-    id: "MEM022",
+    id: "MEM023",
     title: "Group Selfie",
     category: "Last Training Day",
     url: "/pic/ab6.jpeg"
   },
   {
-    id: "MEM023",
+    id: "MEM024",
     title: "Group Selfie 2",
     category: "LastTraining Day",
     url: "/pic/ab7.jpeg"
   },
   {
-    id: "MEM024",
+    id: "MEM025",
     title: "Group Selfies 3",
     category: "Last Training Day",
     url: "/pic/ab5.jpeg"
   },
   {
-    id: "MEM025",
+    id: "MEM026",
     title: "Emotional Moment 🥹",
     category: "Akashta ko Floor pe Nahi Jana hai",
     url: "/pic/ak1.jpeg"
   },
   {
-    id: "MEM026",
+    id: "MEM027",
     title: "Emotional Moment 😭",
     category: "Training khatam hua hai, zindagi nahi 😭",
     url: "/pic/ak.jpeg"
   }
   ,
   {
-    id: "MEM027",
+    id: "MEM028",
     title: "Session Selfie",
     category: "Training khatam hone k baad",
     url: "/pic/floor.jpeg"
-  }
+  },
+  {
+    id: "MEM029",
+    title: "Akashta ka depression",
+    category: "Snaps",
+    url: "/pic/dep.jpg"
+  },
+  {
+    id: "MEM030",
+    title: "Choota Don",
+    category: "Snaps",
+    url: "/pic/ch.jpg"
+  },
+  {
+    id: "MEM031",
+    title: "Spotiify Premium",
+    category: "Snaps",
+    url: "/pic/spo.jpg"
+  },
+  {
+    id: "MEM032",
+    title: "Black Lover",
+    category: "Snaps",
+    url: "/pic/aa.jpg"
+  },
+  {
+    id: "MEM033",
+    title: "NEELU",
+    category: "Snaps",
+    url: "/pic/ne.jpg"
+  },
+  {
+    id: "MEM034",
+    title: "JAPANI",
+    category: "Snaps",
+    url: "/pic/vi.jpg"
+  },
+  
 ];
